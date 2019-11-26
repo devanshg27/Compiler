b41a02c0a33544cd8feb526036bb67ce28153156
Devansh Gautam <devanshg27@gmail.com>
2019-11-27 03:30:57 +0530

    Added no error version of context

--- src/context.h
+++ src/context.h
@@ -57,3 +57,57 @@ void Context<U, T>::resize_context(int sz) {
 
 template <typename U, typename T>
 Context<U, T>::~Context() {}
+
+
+template <typename U, typename T>
+class ContextNoError {
+private:
+    list<pair<U, T>> clist;
+public:
+    ContextNoError();
+    void add_context(pair<U, T>);
+    void add_context(const U&, T&&);
+    int get_context_size();
+    void resize_context(int);
+    T get_value(const U &s, T);
+    ~ContextNoError();
+};
+
+template <typename U, typename T>
+ContextNoError<U, T>::ContextNoError() {}
+
+template <typename U, typename T>
+void ContextNoError<U, T>::add_context(pair<U, T> m) {
+    clist.push_front(m);
+}
+
+template <typename U, typename T>
+void ContextNoError<U, T>::add_context(const U &s, T&& m) {
+    clist.push_front(make_pair(s, std::move(m)));
+}
+
+template <typename U, typename T>
+T ContextNoError<U, T>::get_value(const U &s, T default_val) {
+    for(auto&z: clist) {
+        if(s == z.first) {
+            return z.second;
+        }
+    }
+    return default_val;
+}
+
+template <typename U, typename T>
+int ContextNoError<U, T>::get_context_size() {
+    return clist.size();
+}
+
+template <typename U, typename T>
+void ContextNoError<U, T>::resize_context(int sz) {
+    assert((int)clist.size() >= sz);
+    while(sz != (int)clist.size()) {
+        clist.pop_front();
+    }
+}
+
+template <typename U, typename T>
+ContextNoError<U, T>::~ContextNoError() {}
